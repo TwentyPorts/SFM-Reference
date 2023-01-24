b4e364c10a62bc6cb2a0f1b027f9e9b248e875ae
TwentyPorts <2rayxu@gmail.com>
2023-01-24 14:55:49 -0600

    Initial commit

---
+++ src/gridData.js
@@ -0,0 +1,18 @@
+import Thumb_Valve_Portraits from './Assets/Images/Valve-Style Portraits/0.png';
+import Thumb_Character_Lighting_Headshots from './Assets/Images/Character Lighting Headshots/0.png';
+import Thumb_POV_Punch from './Assets/Images/POV Punch/0.png';
+
+export const gridData=[
+    {
+      "tag": "Valve-Style Portraits",
+      "image": Thumb_Valve_Portraits
+    },
+    {
+      "tag": "Character Lighting Headshots",
+      "image": Thumb_Character_Lighting_Headshots
+    },
+    {
+      "tag": "POV Punch",
+      "image": Thumb_POV_Punch
+    }
+  ];
---
+++ src/imageCredits.js
@@ -0,0 +1,25 @@
+export const imageCredits = [
+  {
+    "Valve-Style Portraits": {
+      0: {
+        author: "ghobleen#6971",
+      },
+      1: {
+        author: "Sharpe-Fan",
+      },
+    },
+    "Character Lighting Headshots": {
+      0: {
+        author: "dusty!#4833",
+      },
+      1: {
+        author: "Flynn#4917",
+      },
+    },
+    "Valve-Style Portraits": {
+      0: {
+        author: "eggsalad#2271",
+      },
+    },
+  },
+];

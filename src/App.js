f7ca5b70453e19531e69dd6a2595ec914e49b96d
TwentyPorts <2rayxu@gmail.com>
2023-02-03 12:44:46 -0600

    Restyle as dark theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import GridView from './GridView/GridView';
-import CarouselWrapper from './Carousel/CarouselWrapper';
-import AboutPage from './About/AboutPage';
-import DefaultNavbar from './Navbar/DefaultNavbar.tsx';
+import { Routes, Route } from "react-router-dom";
+import GridView from "./GridView/GridView";
+import CarouselWrapper from "./Carousel/CarouselWrapper";
+import AboutPage from "./About/AboutPage";
+import DefaultNavbar from "./Navbar/DefaultNavbar.tsx";
 
 const App = () => {
- return (
+  console.log("App rendered");
+  return (
     <>
-       <DefaultNavbar></DefaultNavbar>
-       <Routes>
-          <Route path="/" element={<GridView />} />
-          <Route path="/:id" element={<CarouselWrapper />} />
-          <Route path="/About" element={<AboutPage />} />
-       </Routes>
+      <DefaultNavbar></DefaultNavbar>
+      <Routes>
+        <Route path="/" element={<GridView />} />
+        <Route path="/:id" element={<CarouselWrapper />} />
+        <Route path="/About" element={<AboutPage />} />
+      </Routes>
     </>
- );
+  );
 };
 
 export default App;

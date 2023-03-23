504730569fba17d493a9865ff1982ff559f6fa72
TwentyPorts <2rayxu@gmail.com>
2023-03-23 11:38:40 -0500

    Fix incorrect author attribution

--- src/Carousel/Data/Close-Up_Shots.js
+++ src/Carousel/Data/Close-Up_Shots.js
@@ -1,6 +1,6 @@
 export const carouselData = [
   { author: "Lourny#8567" },
-  { author: "Flynn#4917" },
+  { author: "MajinBro#4297" },
   { author: "LemonManIII#0001" },
   { author: "John Skeleton#0508" },
   { author: "Tem Zilla#6310" },

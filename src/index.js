4ed54d9ade4ecfbc1413324e1039b9dc0c1bfc12
Raymond Xu <2rayxu@gmail.com>
2024-01-11 12:23:08 -0600

    Add basename to router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SFM-Reference">
       <App />
     </BrowserRouter>
   </React.StrictMode>

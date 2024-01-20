bdf3849133dfda617272b22bf7799c921fbfc276
Raymond Xu <2rayxu@gmail.com>
2024-01-20 15:19:41 -0600

    Implement image submission
    Submitting the image will now write the image and its metadata to the project's Firebase Storage.
    The page now displays upload progress, notifies the user when finished, and prompts for page reload.
    Project now uses Firebase Auth.

--- src/Submit/SubmitPage.js
+++ src/Submit/SubmitPage.js
@@ -1,5 +1,5 @@
 import "./SubmitPage.scss";
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
@@ -7,7 +7,18 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import Divider from "@mui/material/Divider";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
 import { gridData } from "../gridData.js";
+
+import firebaseApp from "../Firebase.js";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// Initialize Cloud Firestore
+const storage = getStorage(firebaseApp);
+const auth = getAuth();
+signInWithEmailAndPassword(auth, process.env.REACT_APP_EMAIL, process.env.REACT_APP_PASSWORD);
 
 const theme = createTheme({
   palette: {
@@ -23,13 +34,14 @@ const theme = createTheme({
 
 const SubmitPage = () => {
   document.title = "Submit Images - SFM Reference";
-  const [formData, setFormData] = React.useState({
+  const [formData, setFormData] = useState({
     author: "",
     url: "",
     category: "",
   });
-  const [errorMessage, setErrorMessage] = React.useState("");
-  const [uploadedImage, setUploadedImage] = React.useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [uploadPercent, setUploadPercent] = useState(0);
 
   const handleChange = (event, property) => {
     setFormData({ ...formData, [property]: event.target.value });
@@ -37,7 +49,7 @@ const SubmitPage = () => {
     // check for invalid url
     if (event.target.id && event.target.id.includes("url")) {
       try {
-        const fccUrl = new URL(event.target.value);
+        new URL(event.target.value);
         setErrorMessage("");
       } catch (e) {
         if (e instanceof TypeError) {
@@ -49,6 +61,41 @@ const SubmitPage = () => {
 
   const handleImageUpload = (event) => {
     setUploadedImage(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (uploadedImage == null) {
+      alert("Don't forget to upload an image!");
+      return;
+    }
+
+    const storageRef = ref(storage, `/files/${uploadedImage.name}`);
+    const metadata = {
+      customMetadata: {
+        author: formData.author,
+        url: formData.url,
+        category: formData.category,
+      },
+    };
+    const uploadTask = uploadBytesResumable(
+      storageRef,
+      uploadedImage,
+      metadata
+    );
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+
+        // update progress
+        setUploadPercent(percent);
+      },
+      (err) => console.log(err)
+    );
   };
 
   return (
@@ -64,71 +111,102 @@ const SubmitPage = () => {
         Submit* your artwork to the website!
       </h1>
       <ThemeProvider theme={theme}>
-        <FormControl>
-          <div className="submit-page-form-container">
-            <TextField
-              required
-              id="submit-page-input-author"
-              label="Author"
-              title="Name or username of the artist. List multiple authors separated by commas."
-              value={formData.name}
-              onChange={(e) => handleChange(e, "author")}
-            />
-            <TextField
-              id="submit-page-input-url"
-              label="Source URL"
-              title="Direct link to the artwork (e.g. Twitter, Steam, Reddit). Include the https:// prefix."
-              value={formData.url}
-              onChange={(e) => handleChange(e, "url")}
-              error={errorMessage !== ""}
-              helperText={errorMessage}
-            />
-            <label htmlFor="files">
-              <div className="submit-page-upload-image-container">
-                {uploadedImage ? (
-                  <img
-                    alt="thumbnail"
-                    src={URL.createObjectURL(uploadedImage)}
-                    className="submit-page-upload-image-thumbnail"
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <FormControl>
+            <div className="submit-page-form-container">
+              <label htmlFor="files">
+                <div className="submit-page-upload-image-container">
+                  {uploadedImage ? (
+                    <img
+                      alt="thumbnail"
+                      src={URL.createObjectURL(uploadedImage)}
+                      className="submit-page-upload-image-thumbnail"
+                    />
+                  ) : (
+                    <p className="submit-page-upload-image-text">
+                      Select Image
+                    </p>
+                  )}
+                </div>
+              </label>
+              <input
+                id="files"
+                type="file"
+                accept="image/png, image/jpeg"
+                multiple={true}
+                style={{ display: "none" }}
+                onChange={(e) => handleImageUpload(e)}
+              />
+              {uploadPercent > 0 && (
+                <Box sx={{ width: "100%" }}>
+                  <LinearProgress
+                    variant="determinate"
+                    value={uploadPercent}
+                    color={uploadPercent !== 100 ? "primary" : "success"}
                   />
-                ) : (
-                  <p className="submit-page-upload-image-text">
-                    Select Image or Drag and Drop
+                </Box>
+              )}
+              <TextField
+                required
+                fullWidth
+                id="submit-page-input-author"
+                label="Author"
+                title="Name or username of the artist. List multiple authors separated by commas."
+                value={formData.name}
+                onChange={(e) => handleChange(e, "author")}
+              />
+              <TextField
+                fullWidth
+                id="submit-page-input-url"
+                label="Source URL"
+                title="Direct link to the artwork (e.g. Twitter, Steam, Reddit). Include the https:// prefix."
+                value={formData.url}
+                onChange={(e) => handleChange(e, "url")}
+                error={errorMessage !== ""}
+                helperText={errorMessage}
+              />
+              <TextField
+                required
+                select
+                fullWidth
+                id="submit-page-select"
+                label="Category"
+                title="Must fit into one existing category."
+                defaultValue=""
+                value={formData.category}
+                onChange={(e) => handleChange(e, "category")}
+              >
+                {Object.values(gridData).map((value) => (
+                  <MenuItem key={value.tag} value={value.tag}>
+                    {value.tag}
+                  </MenuItem>
+                ))}
+              </TextField>
+              {uploadPercent !== 100 ? (
+                <Button variant="contained" type="submit">
+                  Submit
+                </Button>
+              ) : (
+                <Button variant="contained" disabled>
+                  Submit
+                </Button>
+              )}
+              {uploadPercent === 100 && (
+                <div className="submit-page-submitted">
+                  <p className="submit-page-submitted-text">
+                    Submitted! Thank you!
                   </p>
-                )}
-              </div>
-            </label>
-            <input
-              required
-              id="files"
-              type="file"
-              accept="image/png, image/jpeg"
-              multiple={true}
-              style={{ display: "none" }}
-              onChange={(e) => handleImageUpload(e)}
-            />
-            <TextField
-              required
-              select
-              fullWidth
-              id="submit-page-select"
-              label="Category"
-              title="Must fit into one existing category."
-              defaultValue=""
-              value={formData.category}
-              onChange={(e) => handleChange(e, "category")}
-            >
-              {Object.values(gridData).map((value) => (
-                <MenuItem key={value.tag} value={value.tag}>
-                  {value.tag}
-                </MenuItem>
-              ))}
-            </TextField>
-            <Button variant="contained" type="submit">
-              Submit
-            </Button>
-          </div>
-        </FormControl>
+                  <Button
+                    variant="outlined"
+                    onClick={() => window.location.reload()}
+                  >
+                    Click to Refresh
+                  </Button>
+                </div>
+              )}
+            </div>
+          </FormControl>
+        </form>
       </ThemeProvider>
       <Divider
         flexItem

393825b33d68bafdb8378e75a277cbdb8b569e99
Raymond Xu <2rayxu@gmail.com>
2024-01-20 17:43:59 -0600

    Remove basename and set homepage for Vercel deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/SFM-Reference">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>

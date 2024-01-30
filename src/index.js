bf1509e4ca988d80c9818d961179b1c661b65d95
Raymond Xu <2rayxu@gmail.com>
2024-01-30 13:09:38 -0600

    Ensure basename

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+if (window.location.pathname === '/') {
+  window.location.assign('/SFM-Reference');
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
